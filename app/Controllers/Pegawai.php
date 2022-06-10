74d131560601ef09a916cc45e46a6845db9e1db5
Hafid Robbi Assifa <hafidrobbi9@gmail.com>
2022-06-10 21:29:16 +0700

    star projek

---
+++ app/Controllers/Pegawai.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Controllers;
+
+class Pegawai extends BaseController
+{
+    function __construct()
+    {
+        $this->model = new \App\Models\ModelPegawai();
+    }
+    public function index()
+    {
+        return view('pegawai_view');
+    }
+}
